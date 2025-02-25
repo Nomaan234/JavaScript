9706508b6d80b7d1e0f2a224d812e779ce36fb15
Nomaan234 <mdnomaan234@gmail.com>
2025-02-26 00:25:59 +0530

    learning JS

---
+++ relational.js
@@ -0,0 +1,14 @@
+let a=prompt("enter value of a");
+let b=prompt("enter value of b");
+
+a = parseFloat(a);   
+b = parseFloat(b);
+
+console.log(" a+b =", a + b );
+console.log(" a-b =", a - b );
+console.log(" a*b =", a * b );
+console.log(" a/b =", a / b );
+console.log(" a%b =", a % b );
+console.log(" a**b =", a ** b );
+console.log(" a++ =", ++a );
+console.log(" b++ =", ++b );
