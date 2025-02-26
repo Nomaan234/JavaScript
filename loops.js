b9bd86cf00352a5d69b92fbdd62094eb748f1522
Nomaan234 <mdnomaan234@gmail.com>
2025-02-26 17:30:22 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,25 @@
+
+// let sum=0;
+
+// let a=prompt("enter an integer : ");
+
+// for(let i=0 ; i<=a ; i++)
+// {
+//     if(i%2===0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// // console.log(sum);
+
+let str ="mohd nomaan atiq";
+
+for( let i of str)
+{
+    console.log( "i : ",i);
+    
+}
+console.log("lenght: ",str.length);
+str[0]="a";
+console.log(str);
--- relational.js
+++ relational.js
@@ -1,14 +1,4 @@
-let a=prompt("enter value of a");
-let b=prompt("enter value of b");
-
-a = parseFloat(a);   
-b = parseFloat(b);
-
-console.log(" a+b =", a + b );
-console.log(" a-b =", a - b );
-console.log(" a*b =", a * b );
-console.log(" a/b =", a / b );
-console.log(" a%b =", a % b );
-console.log(" a**b =", a ** b );
-console.log(" a++ =", ++a );
-console.log(" b++ =", ++b );
+let name="nomaan";
+console.log(name);
+name[0]="m";
+console.log(name);
