35b793c7d65567406310df4189841e8ad03a27ea
Nomaan234 <mdnomaan234@gmail.com>
2025-03-02 16:55:37 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,40 @@
+// console.dir(document.body.childNodes);
+// console.log(document.body.childNodes[0]);
+// console.log(document.body.childNodes[1]);
+// console.log(document.body.childNodes[2]);
+
+// let button = document.getElementById("butt");
+// console.log(button)
+
+
+// let dot = document.getElementsByClassName("heading");
+// console.log(dot)
+
+// let dott = document.getElementsByTagName("p");
+// console.log(dott)
+
+// let butto = document.getElementsByTagName("button");
+// console.log(butto)
+// console.dir(butto)
+
+// let first = document.querySelector("div")
+// console.log(first)
+
+// let all = document.querySelectorAll("div")
+// console.log(all.innerText)
+
+// let newbutton = document.createElement("button")
+// console.log(newbutton)
+// newbutton.style.height="20px"
+// newbutton.style.width="50px"
+// newbutton.innerText="click me"
+// let body = document.querySelector("body")
+
+// // div.append(newbutton)
+// body.prepend(newbutton)
+// // div.before(newbutton)
+// // div.after(newbutton)
+
+let para=document.querySelector(".para")
+console.log(para)
+
